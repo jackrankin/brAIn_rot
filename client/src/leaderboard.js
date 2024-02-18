2edb47178cb854b2904933a577ae5757e874d749
chaitrapirisingula <89547692+chaitrapirisingula@users.noreply.github.com>
2024-02-17 19:37:15 -0600

    psu

--- client/src/leaderboard.js
+++ client/src/leaderboard.js
@@ -6,8 +6,8 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { Button, Typography } from "@mui/material";
+import { useNavigate, useParams } from "react-router-dom";
 import CodeSubmit from "./CodeSubmit";
 
 function createData(name, calories, fat, carbs, protein) {
@@ -22,10 +22,13 @@ const rows = [
   createData("Gingerbread", 356, 16.0, 49, 3.9),
 ];
 
-export default function Leaderboard({ gameName }) {
-  let navigate = useNavigate();
+export default function Leaderboard() {
+  
+  let routeParams = useParams();
+
   return (
     <div>
+      <Typography>{routeParams.name}</Typography>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
