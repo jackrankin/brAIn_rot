f8176321776fa2cb3e16512961e3b64b76c1f74c
chaitrapirisingula <89547692+chaitrapirisingula@users.noreply.github.com>
2024-02-18 03:52:39 -0600

    update starter code

--- client/src/StarterCode.js
+++ client/src/StarterCode.js
@@ -1,9 +1,9 @@
 // Starter code for all games
 const filler_code =
-  "# Write an algorithm to find the next best move for filler.\n# You are given a 2D array board filled with integers 1-8.\ndef filler_move():\n  return 0\n\n\n\n\n\n\n\n\n";
-const rps_code = '# Write the best algorithm for rock, paper, scissors!\ndef rps_move():\n  return "R"\n\n\n';
-const connect4_code = "# Write an algorithm to find the next best move in Connect4.\n# You are given a board containing yours or your opponents pieces.\ndef connect4_move():\n  return 4\n\n\n\n\n\n\n\n\n\n";
-const tron_code = "# Write an algorithm to play tron!\n# The goal is to try to make your opponent hit you without touching walls or yourself.\ndef tron_move():\n  return 4\n\n\n\n\n\n\n\n\n\n";
+  "# Write an algorithm to find the next best move for filler.\n# You are given a 2D array board filled with integers 1-8.\ndef next_move(board, whoami):\n  return 0\n\n\n\n\n\n\n\n\n";
+const rps_code = '# Write the best algorithm for rock, paper, scissors!\ndef next_move():\n  return "R"\n\n\n';
+const connect4_code = "# Write an algorithm to find the next best move in Connect4.\n# You are given a board containing yours or your opponents pieces.\ndef next_move(board):\n  return 4\n\n\n\n\n\n\n\n\n\n";
+const tron_code = "# Write an algorithm to play tron!\n# The goal is to try to make your opponent hit you without touching walls or yourself.\ndef next_move(board, me, opp):\n  return 4\n\n\n\n\n\n\n\n\n\n";
 
 export const code_map = {
   filler: filler_code,
