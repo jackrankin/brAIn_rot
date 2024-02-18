bd6fa0c623b30e4cb4c7d1b7e5f41bc422dd6731
chaitrapirisingula <89547692+chaitrapirisingula@users.noreply.github.com>
2024-02-18 03:02:31 -0600

    todos

--- client/src/StarterCode.js
+++ client/src/StarterCode.js
@@ -1,9 +1,9 @@
 // Starter code for all games
 const filler_code =
-  "# Write a function that finds the best move\ndef filler_move():\n  return 0\n\n\n\n\n\n\n\n\n";
-const rps_code = 'def rps_move():\n  return "R"\n\n\n\n\n\n\n\n\n';
-const connect4_code = "def connect4_move():\n  return 4\n\n\n\n\n\n\n\n\n\n";
-const tron_code = "def tron_move():\n  return 4\n\n\n\n\n\n\n\n\n\n";
+  "# Write an algorithm to find the next best move for filler.\nYou are given a 2D array board filled with integers 1-8 and a whoami variable which represents the turn.\ndef filler_move():\n  return 0\n\n\n\n\n\n\n\n\n";
+const rps_code = '# Write the best algorithm for rock, paper, scissors!\ndef rps_move():\n  return "R"\n\n\n\n\n\n\n\n\n';
+const connect4_code = "# Write an algorithm to find the next best move in Connect4. You are given a board containing yours or your opponents pieces.\ndef connect4_move():\n  return 4\n\n\n\n\n\n\n\n\n\n";
+const tron_code = "# Write an algorithm to play tron!\nThe goal is to try to make your opponent hit you without touching walls or yourself.\ndef tron_move():\n  return 4\n\n\n\n\n\n\n\n\n\n";
 
 export const code_map = {
   filler: filler_code,
